199ef1c005ef3f8c54308059c4d9f839e345299a
CarlosCujcuj <carloscy@ufm.edu>
2022-03-30 19:02:08 -0600

    Changes to Pathfinder

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -6,41 +6,46 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour{
 
 
-    [SerializeField] List<Waypoint> path = new List<Waypoint>();
     [SerializeField] [Range(0f, 5f)] float speed = 1f;
+    List<Node> path = new List<Node>();
 
     Enemy enemy;
+    GridManager gridManager;
+    Pathfinder pathfinder;
 
     // Called whenever an object is enable  
     void OnEnable(){ 
-        FindPath();
+
         ReturnToStart();
+        RecalculatePath(true);
+        
+    }
+
+    void Awake(){
+        enemy = GetComponent<Enemy>();
+        gridManager = FindObjectOfType<GridManager>();
+        pathfinder = FindObjectOfType<Pathfinder>();
+
+    }
+
+    void RecalculatePath(bool resetPath){
+
+        Vector2Int coordinates = new Vector2Int();
+
+        if(resetPath){
+            coordinates = pathfinder.StartCoordinates;
+        }else{
+            coordinates = gridManager.GetCoordinatesFromPosition(transform.position);
+        }
+        
+        StopAllCoroutines();
+        path.Clear();
+        path = pathfinder.GetNewPath(coordinates);
         StartCoroutine(FollowPath());
     }
 
-    void Start(){
-        enemy = GetComponent<Enemy>();
-    }
-
-    void FindPath(){
-
-        path.Clear();
-
-        GameObject parent = GameObject.FindGameObjectWithTag("Path");
-
-
-        foreach(Transform child in parent.transform){
-
-            Waypoint waypoint = child.GetComponent<Waypoint>();
-            
-            if(waypoint != null){
-                path.Add(waypoint);
-            }
-        }
-    }
-
     void ReturnToStart(){
-        transform.position = path[0].transform.position;
+        transform.position = gridManager.GetPositionFromCoordinates(pathfinder.StartCoordinates);
     }
 
     void FinishPahth(){
@@ -52,10 +57,10 @@ public class EnemyMover : MonoBehaviour{
 
     IEnumerator FollowPath(){
 
-        foreach(Waypoint waypoint in path){
+        for(int i = 1; i < path.Count; i++){ // 1 asi empieza con el siguiente Tile y no el current
             
             Vector3 startPosition = transform.position;
-            Vector3 endPosition = waypoint.transform.position;
+            Vector3 endPosition = gridManager.GetPositionFromCoordinates(path[i].coordinates);
 
             float travelPercent = 0f;
 
